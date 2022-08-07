ca1e76a14d62530194ac7988bcf6fcb21d208379
Am1r-B_PC <69463011+Am1r-B@users.noreply.github.com>
2022-08-08 02:35:47 +0430

    setting colors

--- life/main.lua
+++ life/main.lua
@@ -1,9 +1,14 @@
+function love.load()
+  love.graphics.setBackgroundColor(1, 1, 1)
+end
+
 function love.draw()
   for y = 1, 50 do
     for x = 1, 70 do
       local cellSize = 5
       local cellDrawSize = cellSize - 1
       
+      love.graphics.setColor(.86, .86, .86)
       love.graphics.rectangle(
         'fill',
         (x - 1) * cellSize,
