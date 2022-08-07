91d567b8309bd617f67f91b9887474452dfc307b
Am1r-B_PC <69463011+Am1r-B@users.noreply.github.com>
2022-08-08 02:33:26 +0430

    drawing all the cells

--- life/main.lua
+++ life/main.lua
@@ -1,14 +1,16 @@
 function love.draw()
-  for x = 1, 70 do
-    local cellSize = 5
-    local cellDrawSize = cellSize - 1
-    
-    love.graphics.rectangle(
-      'fill',
-      (x - 1) * cellSize,
-      0,
-      cellDrawSize,
-      cellDrawSize
-    )
+  for y = 1, 50 do
+    for x = 1, 70 do
+      local cellSize = 5
+      local cellDrawSize = cellSize - 1
+      
+      love.graphics.rectangle(
+        'fill',
+        (x - 1) * cellSize,
+        (y - 1) * cellSize,
+        cellDrawSize,
+        cellDrawSize
+      )
+    end
   end
 end
